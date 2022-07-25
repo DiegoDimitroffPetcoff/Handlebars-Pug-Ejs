84456edef8f608e092791da7d8da3276a492afa4
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-07-25 19:04:08 -0300

    setea PORT heroku

--- engine-ejs/server.js
+++ engine-ejs/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
